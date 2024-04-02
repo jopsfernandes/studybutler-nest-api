1413e59f501786c20170032e5ef23fdbf0d6b42f
jopsfernandes <joaopfernandessantos@gmail.com>
2024-04-02 11:03:39 -0400

    feat: handle request body

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,28 +1,32 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { PrismaService, } from './database/prisma.service'
+import {randomUUID} from 'node:crypto'
 
 
 @Controller()
 export class AppController {
-  constructor(private prisma: PrismaService){}
- 
+  constructor(private prisma: PrismaService) { }
+
 
   @Post()
-  async getHello(){
+  async getHello(@Body() body:any) {
+
+    const{} = body
+     
     const user = await this.prisma.user.create({
-      data:{
-        id: 231,
+      data: {
+        id: 123,
         name: 'Capitão Jacksparrow',
         email: 'capitao@gmail.com',
         password: 'qualquersenha123',
       }
     })
 
-    return{
+    return {
       user,
     }
   }
-  
 
-  
+
+
 }
